32ab5be13305a5c7e18f2b31dd69cad8e5752415
Josh Weeks <weeks.josh@gmail.com>
2016-05-03 22:39:58 -0400

    Basic structure established / some unneeded files removed

---
+++ Puffincast.Processing/ICommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puffincast.Processing
+{
+    public interface ICommandHandler
+    {
+        Task<string> Execute(string user, string command);
+    }
+
+    public class CommandHandler : ICommandHandler
+    {
+        public Task<string> Execute(string user, string command)
+        {
+            return Task.FromResult("Send the mothership.");
+        }
+    }
+}
---
+++ Puffincast/Controllers/PuffController.cs
@@ -0,0 +1,28 @@
+﻿using Puffincast.Processing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Puffincast.Controllers
+{
+    public class PuffController : ApiController
+    {
+        private ICommandHandler commandHandler;
+
+        public PuffController() : this(null) { }
+
+        public PuffController(ICommandHandler commandHandler)
+        {
+            this.commandHandler = commandHandler ?? new CommandHandler();
+        }
+
+        public async Task<string> Get(string user_name, string text)
+        {
+            return await this.commandHandler.Execute(user_name, text);
+        }
+    }
+}
