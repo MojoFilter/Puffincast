67ea23d9115b819fe93c9e5c606960b682cc0128
Josh Weeks <weeks.josh@gmail.com>
2016-05-27 12:36:06 -0400

    Set up library projects

---
+++ Puffincast.Library.Core/LibraryScanner.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puffincast.Library.Core
+{
+    public class LibraryScanner
+    {
+        private static readonly HashSet<string> extensions = new HashSet<string>(
+            typeof(TagLib.SupportedMimeType).Assembly.GetExportedTypes()
+            .SelectMany(t => t.GetCustomAttributes(false))
+            .OfType<TagLib.SupportedMimeType>()
+            .Where(m => !string.IsNullOrWhiteSpace(m.Extension))
+            .Select(m => m.Extension));
+
+        private IEnumerable<string> DiscoverFiles(string path) =>
+            new DirectoryInfo(path).EnumerateFiles("*", SearchOption.AllDirectories)
+                .Where(f => !string.IsNullOrWhiteSpace(f.Extension) && extensions.Contains(f.Extension.Substring(1)))
+                .Select(f => f.FullName);
+
+        private Task<int> ProcessFile(TagLib.File file) => Task.FromResult(0);
+
+        public IObservable<ScanProgress> Scan(string path) => Observable.Create<ScanProgress>(obs =>
+            {
+                var fileList = this.DiscoverFiles(path).ToList();
+                var files = fileList
+                .Select((filename, index) => new ScanProgress(index / (double)fileList.Count, filename))
+                .ToObservable()
+                .Publish()
+                .RefCount();
+
+                var processing = files
+                    .Select(f => f.Status)
+                    .Select(filename =>
+                    {
+                        TagLib.File file = null;
+                        bool success = false;
+                        try
+                        {
+                            file = TagLib.File.Create(filename);
+                            success = true;
+                        }
+                        catch (TagLib.UnsupportedFormatException) { }
+                        return new { file, success };
+                    })
+                    .Where(x => x.success)
+                    .Select(x => x.file)
+                    .SelectMany(this.ProcessFile);
+
+                return new CompositeDisposable(
+                        files.Subscribe(obs.OnNext, obs.OnError),
+                        processing.Subscribe(_ => { }, obs.OnCompleted));
+            });
+
+
+
+        public struct ScanProgress
+        {
+            public double Percent { get; }
+            public string Status { get; }
+            public ScanProgress(double percent, string status)
+            {
+                this.Percent = percent;
+                this.Status = status;
+            }
+        }
+
+    }
+}
---
+++ Puffincast.Library.Interactive/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Puffincast.Library.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Puffincast.Library.Interactive
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IDisposable s;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string lib = @"C:\Users\jweeks\Music";
+
+            this.s = new LibraryScanner().Scan(lib)
+                .Select(p => $"[{(int)(p.Percent * 100.0)}]% {System.IO.Path.GetFileNameWithoutExtension(p.Status)}")
+                .Subscribe(p => Debug.WriteLine(p), e => Debug.WriteLine(e.Message));
+        }
+    }
+}
